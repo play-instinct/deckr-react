e409c3ece9035093afacf455d62c7db9ec7945d1
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2019-03-11 10:05:43 -0700

    comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
 import cardBack from './card-back.png';
-import Background from './wood_pattern.png';
+import axios from 'axios';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 
-import axios from 'axios'
 
+//post-code/refactor thoughts: 
+
+
+// i approched this with as much simplicity in mind and decided it was easier to just set up a 1 stateful
+//component as opposed to multipls with passing props. another way to structure this would be to have 3 separate components 
+//for the cards, the card-board, and the button, and use redux for better API response management
+
+
+//I am unsatisifed with the resulting css transition effect - I need to read more about proper management of css transitions with react
+//transition groups (https://reactjs.org/docs/animation.html)
+
+
+
+//create intial state to clear data easily 
 
 const initialState = {
   current_cards: [],
@@ -26,6 +40,8 @@ class App extends Component {
     this.loadBoard();
   }
 
+  //could also set to intiial state
+
   loadBoard() {
     this.setState({
         error: null,
@@ -33,6 +49,10 @@ class App extends Component {
         current_cards: []
     });
   }
+
+  //simple conditional that checks for whether this action was triggered before/
+  //if the state was changed
+
 
   handleClick () {
     if (this.state == initialState) {
@@ -55,6 +75,8 @@ class App extends Component {
    
   }
 
+   //simple conditional that checks for an error state from api, 'loading state' (not really best terminology here)
+
   render () {
     let body;
         if (this.state.error) {
@@ -66,6 +88,8 @@ class App extends Component {
                 <div className="message message-default">Click on the button to load 5 cards...</div>
             );
         } else {
+             //if there is no error, some data is here from the API, lets loop thru our state.cards and make some markup
+
               body = (
                 <div className="card-container">
                      {this.state.current_cards.map(card => (
